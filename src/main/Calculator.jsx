b3662ad384e870aaaa07f2245e368f0733683aa8
Marcos Felipe <p.marcosfelipe@gmail.com>
2020-01-10 21:57:03 -0300

    Implementing the logic of the calculator

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -3,9 +3,19 @@ import './Calculator.css';
 
 import Button from '../components/Button';
 import Display from '../components/Display';
+
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+};
 export default class Calculator extends Component {
+  state = { ...initialState };
+
   clearMemory() {
-    console.log('clear');
+    this.setState({ ...initialState });
   }
 
   setOperation(operation) {
@@ -13,7 +23,23 @@ export default class Calculator extends Component {
   }
 
   addDigit(digit) {
-    console.log('digit', digit);
+    if (digit === '.' && this.state.displayValue.includes('.')) return;
+
+    const clearDisplay =
+      this.state.displayValue === '0' || this.state.clearDisplay;
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+    const displayValue = currentValue + digit;
+    this.setState({ displayValue, clearDisplay: false });
+
+    if (digit !== '.') {
+      const index = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[index] = newValue;
+      this.setState({ values });
+      console.log('values ', values);
+    }
   }
 
   render() {
@@ -22,7 +48,7 @@ export default class Calculator extends Component {
 
     return (
       <div className="calculator">
-        <Display value={100}></Display>
+        <Display value={this.state.displayValue}></Display>
         <Button label="AC" click={() => this.clearMemory()} triple></Button>
         <Button
           label="/"
@@ -54,7 +80,7 @@ export default class Calculator extends Component {
           operation
         ></Button>
         <Button label="0" click={() => addDigit(0)} double></Button>
-        <Button label="." click={() => this.setOperation('.')}></Button>
+        <Button label="." click={() => addDigit('.')}></Button>
         <Button label="=" click={() => addDigit('=')} operation></Button>
       </div>
     );
